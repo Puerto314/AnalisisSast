151cbf52cc67ffc99858c1b07fe9ce4a1e669bcd
Esteban <estebanpuertorojas235777@gmail.com>
2026-05-13 12:33:49 -0500

    Pull3
    Pull3

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,29 @@
-import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import {Component, signal} from '@angular/core';
+import {RouterOutlet} from '@angular/router';
+import {FormsModule} from '@angular/forms';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, FormsModule],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App {
+
   protected readonly title = signal('AnalisisSast');
+
+  usuario: string = '';
+  password: string = '';
+  mensaje: string = '';
+
+  validar() {
+
+    if (this.usuario === this.password) {
+      this.mensaje = 'Registrado';
+    } else {
+      this.mensaje = 'Error';
+    }
+
+  }
+
 }
